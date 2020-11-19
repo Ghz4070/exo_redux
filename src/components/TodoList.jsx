5f6bc60f807f6fb312b0354b088c09b956194cf0
Ilies Ghezal <ghezal-ilies@outlook.fr>
2020-11-19 10:26:13 +0100

    second commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 import { connect } from "react-redux";
+import { UPDATE_TODO_ACTION } from "../store/todosReducer";
 
-const TodoItem = ({ todo, onToggle }) => {};
+function TodoItem({ todo, onToggle }) {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => onToggle(todo)}
+        />
+      </label>
+    </li>
+  );
+}
 
-function TodoList({ todos, onToggle }) {
+export function TodoList({ todos, onToggle }) {
+  console.log(todos);
   return (
     <ul>
-      {todos.map((todo) => (
-        <TodoItem todo={todo} onToggle={onToggle} key={todo.id} />
-      ))}
+      {todos.map((todo) => {
+        return <TodoItem todo={todo} onToggle={onToggle} key={todo.id} />;
+      })}
     </ul>
   );
 }
 
-const TodoListStore = connect((state) => ({
-  todos: state.todos,
-}))(TodoList);
+export const TodoListStore = connect(
+  (state) => ({
+    todos: state.todos,
+  }),
+  (dispatch) => ({
+    onToggle: (todo) =>
+      dispatch({
+        type: UPDATE_TODO_ACTION,
+        payload: { ...todo, completed: !todo.completed },
+      }),
+  })
+)(TodoList);
 
 export default TodoList;
